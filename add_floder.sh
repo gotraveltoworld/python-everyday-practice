dde5b8cc7030c6fa1b23926701136089ea814a3c
thlin <tung@mr6inc.com>
2018-02-21 11:23:38 +0800

    Add new script for loop function

--- add_floder.sh
+++ add_floder.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-now=$(date -v -0d '+%Y%m%d')
-mkdir -p everyday/$now
-touch everyday/$now/example.md
+# for date in $(seq 1 10)
+# do
+
+#     # mkdir -p everyday/$now
+#     # touch everyday/$now/example.md
+#     echo $date
+# done
+
+for (( i=1; i<=10; i=i+1 ))
+do
+    now=$(date -v -${i}d '+%Y%m%d')
+    if test -e everyday/$now; then
+        echo $now', Exist'
+    else
+        echo $now', No Exist'
+        mkdir -p everyday/$now
+        touch everyday/$now/example.md
+    fi
+done
